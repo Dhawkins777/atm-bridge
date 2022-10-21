7a26270671d0b2735c39af752bbd24381aaa7196
Dhawkins777 <dhawkins314@gmail.com>
2022-10-21 16:24:13 -0400

    first commit

---
+++ src/main/test/java/AtmTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AtmTest {
+
+        //Arrange
+        Atm underTest = new Atm(50, "1234");
+    @Test
+    public void shouldHaveADefaultBalanceOf50(){
+        //Act
+        int actualBalance = underTest.getBalance();
+        //Assert
+        assertEquals( 50, actualBalance);
+    }
+    @Test
+    public void shouldHaveADefaultBalanceOf100(){
+        //Arrange
+        Atm underTest = new Atm(100, "1234");
+        //Act
+        int actualBalance = underTest.getBalance();
+        //Assert
+        assertEquals( 100, actualBalance);
+    }
+
+    @Test
+    public void depositOf50ShouldIncreaseAnInitialBalanceOf50To100() {
+        underTest.deposit(50);
+        int actualBalance = underTest.getBalance();
+        assertEquals(100, actualBalance);
+    }
+    @Test
+    public void withdrawalOf50shouldDecreaseAnInitialBalanceOf50To0() {
+        underTest.withdraw(50);
+        int actualBalance = underTest.getBalance();
+        assertEquals(0,actualBalance);
+    }
+
+    @Test
+    public void withdrawOf51ShouldReturnOriginalBalanceOf50(){
+        underTest.withdraw(51);
+        int actualBalance = underTest.getBalance();
+        assertEquals(50,actualBalance);
+    }
+    @Test
+    public void shouldAllowUserAccessWithCorrectPin(){
+        boolean accessResult = underTest.accessAccount("1234");
+        assertEquals(true, accessResult);
+    }
+
+    @Test
+    public void shouldDenyUserAccessWithIncorrectPin(){
+        boolean accessResult = underTest.accessAccount("1233");
+        assertEquals(false, accessResult);
+    }
+}
